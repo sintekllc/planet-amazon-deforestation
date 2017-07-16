85f908a7f499ca34f8452e49787ba44e5e29c121
sintekllc <sintek.llc@gmail.com>
2017-07-16 19:56:09 +0300

    Create testpl.py

---
+++ testpl.py
@@ -0,0 +1,137 @@
+import sys
+
+sys.path.append('../src')
+sys.path.append('../tests')
+
+import os
+import gc
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import data_helper
+from keras_helper import AmazonKerasClassifier
+from kaggle_data.downloader import KaggleDataDownloader
+
+competition_name = "planet-understanding-the-amazon-from-space"
+
+train, train_u = "train-jpg.tar.7z", "train-jpg.tar"
+test, test_u = "test-jpg.tar.7z", "test-jpg.tar"
+test_additional, test_additional_u = "test-jpg-additional.tar.7z", "test-jpg-additional.tar"
+test_labels = "train_v2.csv.zip"
+destination_path = "../input/"
+is_datasets_present = False
+
+# If the folders already exists then the files may already be extracted
+# This is a bit hacky but it's sufficient for our needs
+datasets_path = data_helper.get_jpeg_data_files_paths()
+for dir_path in datasets_path:
+    if os.path.exists(dir_path):
+        is_datasets_present = True
+
+if not is_datasets_present:
+    # Put your Kaggle user name and password in a $KAGGLE_USER and $KAGGLE_PASSWD env vars respectively
+    downloader = KaggleDataDownloader(os.getenv("KAGGLE_USER"), os.getenv("KAGGLE_PASSWD"), competition_name)
+    
+    train_output_path = downloader.download_dataset(train, destination_path)
+    downloader.decompress(train_output_path, destination_path) # Outputs a tar file
+    downloader.decompress(destination_path + train_u, destination_path) # Extract the content of the previous tar file
+    os.remove(train_output_path) # Removes the 7z file
+    os.remove(destination_path + train_u) # Removes the tar file
+    
+    test_output_path = downloader.download_dataset(test, destination_path)
+    downloader.decompress(test_output_path, destination_path) # Outputs a tar file
+    downloader.decompress(destination_path + test_u, destination_path) # Extract the content of the previous tar file
+    os.remove(test_output_path) # Removes the 7z file
+    os.remove(destination_path + test_u) # Removes the tar file
+    
+    test_add_output_path = downloader.download_dataset(test_additional, destination_path)
+    downloader.decompress(test_add_output_path, destination_path) # Outputs a tar file
+    downloader.decompress(destination_path + test_additional_u, destination_path) # Extract the content of the previous tar file
+    os.remove(test_add_output_path) # Removes the 7z file
+    os.remove(destination_path + test_additional_u) # Removes the tar file
+    
+    test_labels_output_path = downloader.download_dataset(test_labels, destination_path)
+    downloader.decompress(test_labels_output_path, destination_path) # Outputs a csv file
+    os.remove(test_labels_output_path) # Removes the zip file
+else:
+    print("All datasets are present.")
+
+train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file = data_helper.get_jpeg_data_files_paths()
+labels_df = pd.read_csv(train_csv_file)
+
+from itertools import chain
+labels_list = list(chain.from_iterable([tags.split(" ") for tags in labels_df['tags'].values]))
+labels_set = set(labels_list)
+print("There is {} unique labels including {}".format(len(labels_set), labels_set))
+
+img_resize = (64, 64) # The resize size of each image
+validation_split_size = 0.2
+batch_size = 128
+
+x_train, y_train, y_map = data_helper.preprocess_train_data(train_jpeg_dir, train_csv_file, img_resize)
+# Free up all available memory space after this heavy operation
+gc.collect();
+
+print("x_train shape: {}".format(x_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+
+from tensorflow.contrib.keras.api.keras.callbacks import ModelCheckpoint
+
+filepath="weights.best.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True)
+
+classifier = AmazonKerasClassifier()
+classifier.add_conv_layer(img_resize)
+classifier.add_flatten_layer()
+classifier.add_ann_layer(len(y_map))
+
+train_losses, val_losses = [], []
+epochs_arr = [20, 5, 5]
+learn_rates = [0.001, 0.0001, 0.00001]
+for learn_rate, epochs in zip(learn_rates, epochs_arr):
+    tmp_train_losses, tmp_val_losses, fbeta_score = classifier.train_model(x_train, y_train, learn_rate, epochs, 
+                                                                           batch_size, validation_split_size=validation_split_size, 
+                                                                           train_callbacks=[checkpoint])
+    train_losses += tmp_train_losses
+    val_losses += tmp_val_losses
+classifier.load_weights("weights.best.hdf5")
+print("Weights loaded")
+
+print (fbeta_score)
+del x_train, y_train
+gc.collect()
+
+x_test, x_test_filename = data_helper.preprocess_test_data(test_jpeg_dir, img_resize)
+# Predict the labels of our x_test images
+predictions = classifier.predict(x_test)
+
+del x_test
+gc.collect()
+
+x_test, x_test_filename_additional = data_helper.preprocess_test_data(test_jpeg_additional, img_resize)
+new_predictions = classifier.predict(x_test)
+
+del x_test
+gc.collect()
+predictions = np.vstack((predictions, new_predictions))
+x_test_filename = np.hstack((x_test_filename, x_test_filename_additional))
+print("Predictions shape: {}\nFiles name shape: {}\n1st predictions entry:\n{}".format(predictions.shape, 
+                                                                              x_test_filename.shape,
+                                                                              predictions[0]))
+                                                                             
+# For now we'll just put all thresholds to 0.2 
+thresholds = [0.2] * len(labels_set)
+
+predicted_labels = classifier.map_predictions(predictions, y_map, thresholds)
+tags_list = [None] * len(predicted_labels)
+for i, tags in enumerate(predicted_labels):
+    tags_list[i] = ' '.join(map(str, tags))
+
+final_data = [[filename.split(".")[0], tags] for filename, tags in zip(x_test_filename, tags_list)]
+final_df = pd.DataFrame(final_data, columns=['image_name', 'tags'])
+final_df.to_csv('../submission_file.csv', index=False)
+classifier.close()
